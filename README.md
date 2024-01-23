54cbdb738972cb7446a5967cf9202e5a74df3d84
masumamrutiya <masumamrutiya29@gmail.com>
2024-01-23 11:52:46 +0530

    hello

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # project-7
+![Alt text](<Screenshot 2024-01-23 110440.png>)
+![Alt text](<Screenshot 2024-01-23 110714.png>)
---
+++ index.c
@@ -0,0 +1,65 @@
+    
+        // question-1
+
+        
+// float average(float a[], int n);
+
+// int main()
+
+// {
+//     float a[] = {6, 7, 8, 9, 5};
+    
+//     int n = sizeof(a) / sizeof(a[0]);
+//     float avg = average(a, n);
+    
+//     printf("The average of  *%.2f*.", avg);
+//     return 0;
+// }
+
+// float average(float a[], int n)
+// {
+//     int i;
+//     float sum = 0.0;
+//     for (i = 0; i < n; i++)
+    
+//     {
+//         sum += a[i];
+//     }
+    
+//     return sum / n;
+// }  
+    
+    
+    
+    //   question-2
+
+
+// #include <stdio.h>
+
+// void printNumbers(int n) {
+//     if (n > 0) {
+//         printNumbers(n - 1);
+//         printf("%d ", n);
+//     }
+// }
+
+// int main() {
+//     printNumbers(10);
+//     return 0;
+// }
+
+
+// question-3
+
+// #include <stdio.h>
+// #include <string.h>
+// int main()
+// {
+//     char name[]="RED";
+//     printf("enter your color name");
+//     scanf("%s",&name);
+//     printf("%s",strupr(name));
+
+//     return 0;
+// }
+
